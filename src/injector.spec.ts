d3d4b87df0283ff58a3e659e86cf85d3aba004e9
Kyle Pfromer <kpfromer2@gmail.com>
2018-04-24 13:28:01 -0600

    Update injector spec

--- src/injector.spec.ts
+++ src/injector.spec.ts
@@ -1,23 +1,142 @@
-import { Injector } from './injector';
+import 'reflect-metadata';
+import * as inject from './inject';
+import * as provider from './provider';
+import { InjectorItem } from './injector';
+import { getInjectMetadataForParam, Inject } from './inject';
 
 describe('Injector', () => {
-  describe('resolve', () => {
-    // it('stuff');
+  let injector: InjectorItem;
+
+  beforeEach(() => {
+    injector = new InjectorItem();
   });
 
-  describe('resolveInjectParameters', () => {});
+  describe('resolve', () => {
+    it('should recursively get dependencies', () => {
+      class Foo {
+        constructor() {}
+      }
 
-  describe('getInjectMetadataForParam', () => {});
+      const decotator = () => (target) => {};
 
-  describe('getInjectMetadata', () => {});
+      @decotator()
+      class Foobar {
+        constructor(/* Inject CoolToken */ public coolToken: string, private foo: Foo) {}
+        getName() {
+          return `Hello ${this.coolToken}`;
+        }
+      }
 
-  describe('resolveArray', () => {});
+      injector.addService(Foobar);
 
-  describe('resolveStringToken', () => {});
+      jest.spyOn(injector, 'resolveInjectParameters').mockImplementation(() => {
+        return (token, index) => {
+          if (index === 0) {
+            return 'cool token is cool';
+          }
 
-  describe('addService', () => {});
+          return token;
+        }
+      });
 
-  describe('addToken', () => {
-    it('should add provider token to tokenProviders map', () => {});
+      jest.spyOn(injector, 'resolveArray').mockImplementation(() => {
+        return (token, index) => {
+          if (index === 0) {
+            return token;
+          }
+
+          return new token();
+        }
+      });
+
+      const value = injector.resolve<Foobar>(Foobar);
+
+      expect(value).toBeInstanceOf(Foobar);
+      expect(value.getName()).toBe('Hello cool token is cool')
+    });
+  });
+
+  describe('resolveInjectParameters', () => {
+    it('should return @Inject token names', () => {
+      jest.spyOn(inject, 'getInjectMetadataForParam').mockReturnValue('CoolToken');
+
+      class TokenClass {
+        constructor(iWillBeInjected: string) {}
+      }
+      injector.addService(TokenClass);
+
+      const value = injector.resolveInjectParameters(TokenClass)(String, 0);
+
+      expect(inject.getInjectMetadataForParam).toHaveBeenCalledWith(TokenClass, 0);
+      expect(value).toBe('CoolToken');
+    });
+
+    it('should return Service if it is not @Inject', () => {
+      jest.spyOn(inject, 'getInjectMetadataForParam').mockReturnValue(undefined);
+
+      // In reality this should be added to injector, but in this test it is not needed
+      class CoolService {}
+
+      class TokenClass {
+        constructor(iWillBeInjected: CoolService) {}
+      }
+      injector.addService(TokenClass);
+
+      const value = injector.resolveInjectParameters(TokenClass)(CoolService, 0);
+
+      expect(inject.getInjectMetadataForParam).toHaveBeenCalledWith(TokenClass, 0);
+      expect(value).toBe(CoolService);
+    });
+  });
+
+  describe('resolveArray', () => {
+    it('should resolve a provider', () => {
+      const mockProvider = jest.fn().mockReturnValue('Hello World!');
+
+      jest.spyOn(provider, 'Provider').mockImplementation(() => {
+        return {
+          name: 'tokenname',
+          getValue: mockProvider
+        }
+      });
+
+      injector.addToken({
+        provide: 'tokenname',
+        useValue: 'Hello World!'
+      });
+
+      const value = injector.resolveArray()('tokenname', 0);
+
+      expect(mockProvider).toHaveBeenCalled();
+      expect(value).toBe('Hello World!');
+    });
+
+    it('should resolve a service', () => {
+      class AService {
+        constructor(public name: string) {}
+      }
+
+      const service = new AService('hello, world');
+
+      jest.spyOn(injector, 'resolve').mockReturnValue(service);
+
+      injector.addService(AService);
+
+      const value = injector.resolveArray()(AService, 0);
+
+      expect(injector.resolve).toHaveBeenCalledWith(AService);
+      expect(value).toBe(service);
+    });
+
+    it('should throw error if there is no service found', () => {
+      const handler = () => {
+
+        class NotAService {}
+
+        injector.resolveArray()(new NotAService(), 1);
+      };
+
+      expect(handler).toThrowError('[1] Unresolved parameter of type: NotAService');
+    });
   });
 });
--- src/injector.ts
+++ src/injector.ts
@@ -1,15 +1,13 @@
-import { getInjectMetadataForParam, InjectKey } from './inject';
+import { getInjectMetadataForParam} from './inject';
 import { TokenProvider } from './types/token.provider';
 import { Provider } from './provider';
 
-export const Injector = new class {
+// TODO: find a better name
+export class InjectorItem {
   // A map where all registered services will be stored
   // services used only for documentation?
   protected services = new Map<string, Type<any>>();
   protected tokenProviders = new Map<string | Type<any>, Provider<any>>();
-  // protected classProviders = new Map<Type<any>, ClassProvider<any>>();
-  // protected valueProviders = new Map<string, ValueProvider<any>>();
-  // protected factoryProviders = new Map<string, FactoryProvider<any>>();
 
   // resolving instances
   resolve<T>(target: Type<any>): T {
@@ -23,7 +21,7 @@ export const Injector = new class {
     return new target(...injections);
   }
 
-  private resolveInjectParameters(
+  resolveInjectParameters(
     target
   ): (token: any, index: number) => string | Type<any> {
     return (token, index) => {
@@ -37,14 +35,6 @@ export const Injector = new class {
 
   resolveArray() {
     return (token, index) => {
-      // if (isString(token)) {
-      //   return this.resolveStringToken(token);
-      // }
-      //
-      // if (this.classProviders.get(token)) {
-      //   return Injector.resolve<any>(this.classProviders.get(token).useClass);
-      // }
-
       const tokenProvider = this.tokenProviders.get(token);
 
       if (tokenProvider) {
@@ -53,24 +43,13 @@ export const Injector = new class {
 
       if (!this.services.get(token.name)) {
         throw new TypeError(
-          `[${index}] Unresolved parameter of type: ${token.name}`
+          `[${index}] Unresolved parameter of type: ${token.constructor.name}`
         );
       }
 
-      return Injector.resolve<any>(token);
+      return this.resolve<any>(token);
     };
   }
-
-  // private resolveStringToken(tokenString) {
-  //   if (this.valueProviders.get(tokenString)) {
-  //     return this.valueProviders.get(tokenString).useValue;
-  //   } else if (this.factoryProviders.get(tokenString)) {
-  //     const deps = this.factoryProviders.get(tokenString).deps.map(this.resolveArray());
-  //     return this.factoryProviders.get(tokenString).useFactory(...deps);
-  //   } else {
-  //     throw new Error(`No such token found: ${tokenString}`);
-  //   }
-  // }
 
   // store services within the injector
   addService(target: Type<any>) {
@@ -78,21 +57,9 @@ export const Injector = new class {
   }
 
   addToken(tokenProvider: TokenProvider<any>) {
-    // if (instanceOfValueProvider(tokenProvider)) {
-    //   this.valueProviders.set(tokenProvider.provide, tokenProvider);
-    // } else if (instanceOfClassProvider(tokenProvider)) {
-    //   this.classProviders.set(tokenProvider.provide, tokenProvider);
-    // } else if (instanceOfFactoryProvider(tokenProvider)) {
-    //
-    //   if (!tokenProvider.deps) {
-    //     tokenProvider.deps = [];
-    //   }
-    //
-    //   this.factoryProviders.set(tokenProvider.provide, tokenProvider);
-    // } else {
-    //   throw new Error('Unknown token type!');
-    // }
     const provider = new Provider(tokenProvider);
     this.tokenProviders.set(provider.name, provider);
   }
-}();
+}
+
+export const Injector = new InjectorItem();
