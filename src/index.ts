b4aa5a662236470e035b6ff0421ae930207c9fa3
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-09 14:57:53 -0600

    Update package for npm publish

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import 'reflect-metadata';
 
-export * from './service/service';
-export * from './injector/injector';
 export * from './inject/inject';
+
+export * from './module/module';
+export * from './module/module.loader';
+
+export * from './service/service';
+
+export * from './controller/controller';
